670b711fd85e7c9b924f38a8f59b6c27a203b202
Александр Серов <sashaserov2010@gmail.com>
2021-09-29 21:18:06 +0300

    feat(tests): add e2e tests

---
+++ cypress/integration/main-page/main-page.spec.ts
@@ -0,0 +1,9 @@
+describe('main page', () => {
+    beforeEach(() => {
+        cy.visit('/');
+    })
+
+    it('should work', () => {
+        cy.findByText('Hello world').should('exist');
+    })
+})
---
+++ cypress/plugins/index.ts
@@ -0,0 +1,22 @@
+const webpackPreprocessor = require('@cypress/webpack-preprocessor');
+
+module.exports = (on, config) => {
+  const webpackOptions = {
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          loader: 'ts-loader',
+          options: {
+            transpileOnly: true,
+          }
+        }
+      ]
+    }
+  }
+  on('file:preprocessor', webpackPreprocessor({ webpackOptions }))
+
+}
