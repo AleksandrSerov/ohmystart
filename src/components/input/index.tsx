ac97d0e41a254d558455a3a9c1c2e42fd83ee3d8
Александр Серов <sashaserov2010@gmail.com>
2021-11-02 23:25:18 +0300

    feat(*): add classnames dep

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,5 +1,10 @@
 import * as styles from './index.module.css';
+import cn from 'classnames';
 
-export const Input: React.FC = () => {
-	return <input className={styles.input} />;
+export type InputProps = {
+	className?: string;
+};
+// eslint-disable-next-line react/prop-types
+export const Input: React.FC<InputProps> = ({ className }) => {
+	return <input className={cn(styles.input, className)} />;
 };
