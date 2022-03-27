cf880ff61583141bfa6b3b6ae0781f2083ce3edf
Aleksandr Serov <sashaserov2010@gmail.com>
2022-03-27 16:29:34 +0300

    feat(*): add ts plugins for css

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,13 +1,11 @@
 import cn from 'classnames';
 
-import * as styles from './index.module.css';
+import styles from './index.module.css';
 
 export type InputProps = {
 	className?: string;
 };
 
 export const Input: React.FC<InputProps> = ({ className }) => {
-	console.log(styles);
-	console.log(className);
 	return <input className={cn(styles.input, className)} />;
 };
