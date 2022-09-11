33e563b1eb15728704fddfcb72c962bb6ebd4b06
Aleksandr Serov <sashaserov2010@gmail.com>
2022-09-11 23:17:00 +0300

    fix(e2e): fix lint

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,15 +1,18 @@
-import { defineConfig } from 'cypress'
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/extensions */
+/* eslint-disable @typescript-eslint/no-var-requires */
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  video: false,
-  modifyObstructiveCode: false,
-  e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.ts')(on, config)
-    },
-    baseUrl: 'http://localhost:8080',
-    specPattern: 'cypress/e2e/**/*.spec.ts',
-  },
-})
+	video: false,
+	modifyObstructiveCode: false,
+	e2e: {
+		// We've imported your old cypress plugins here.
+		// You may want to clean this up later by importing these.
+		setupNodeEvents(on, config) {
+			return require('./cypress/plugins/index.ts')(on, config);
+		},
+		baseUrl: 'http://localhost:8080',
+		specPattern: 'cypress/e2e/**/*.spec.ts',
+	},
+});
